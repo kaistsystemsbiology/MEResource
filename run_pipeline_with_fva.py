730b56e08a1631e481ae388e1f2da5304b8f7dc1
anlito <anlito@kaist.ac.kr>
2024-05-29 16:33:31 +0900

    pipeline for yeast models

--- run_pipeline_with_fva.py
+++ run_pipeline_with_fva.py
@@ -21,9 +21,12 @@ from baebra.yieldfinder import predict_cofactor_reactions
 from baebra.yieldfinder import validate_swap_target_fva
 
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.StreamHandler()]
+                    )
+
 logger = logging.getLogger('YieldPipeline')
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
 
 
 if __name__ == '__main__':
@@ -72,9 +75,6 @@ if __name__ == '__main__':
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-
-    stream_handler = logging.StreamHandler()
-    logger.addHandler(stream_handler)
 
     model = read_sbml_model(model_dir)
     # manual curation of target model
@@ -129,6 +129,12 @@ if __name__ == '__main__':
 
     # heterologous reaction finder
     logger.info('Finding Heterologous reaction targets ...')
+    manually_curated_rxns = [
+        'MDHy', 'HADPCOADH', 'NAD_H2',
+        'HYDFDN', 'HYDFDN2r', 'HYDFDi',
+        'FAO1', 'FAO2', 'FAO3', 'FAO4', 'FAO10', 'FAO11', 
+    ]
+
     universal_model = load_json_model(universal_model_dir)
     rm_reactions = []
     for each_reaction in universal_model.reactions:
@@ -143,6 +149,10 @@ if __name__ == '__main__':
             pass
         else:
             rm_reactions.append(each_reaction)
+
+        if each_reaction.id in manually_curated_rxns:
+            rm_reactions.append(each_reaction)
+    
 
     logger.info('No. of removed reactions in universal model: %d'%(len(rm_reactions)))
     universal_model.remove_reactions(rm_reactions)
